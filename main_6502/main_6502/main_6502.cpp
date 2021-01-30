6fe88e2784c83c9161192db7ce5e2481643704c4
William Hoke <hokewilliam0@gmail.com>
2021-01-29 17:13:10 -0800

    Initial commit

---
+++ main_6502/main_6502/main_6502.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+
+struct Mem {
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	Byte Data[MAX_MEM];
+	
+	void Initialise() {
+		for (u32 i = 0; i < MAX_MEM; i++) {
+			Data[i] = 0;
+		}
+	}
+
+	/** read 1 byte */
+	Byte operator[](u32 Address) {
+		//asset here Address is < MAX_MEM 
+		return Data[Address];
+	}
+
+	/** write 1 byte */
+	Byte& operator[](u32 Address) {
+		//assert here Address is < MAX_MEM
+		return Data[Address];
+	}
+
+};
+
+struct CPU {
+	Word ProgramCounter;			//program counter
+	Word StackPointer;	//stack pointer
+
+	Byte A, X, Y;		//registers
+
+	Byte C : 1;			//status flag
+	Byte Z : 1;			//status flag
+	Byte I : 1;			//status flag
+	Byte D : 1;			//status flag
+	Byte B : 1;			//status flag
+	Byte V : 1;			//status flag
+	Byte N : 1;			//status flag
+
+	void Reset(Mem& memory) {
+		ProgramCounter = 0xFFFC;
+		StackPointer = 0x0100;
+		C= D = Z = I = B = V = N = 0;
+		A = X = Y = 0;
+		memory.Initialise();
+	}
+
+	Byte FetchByte(u32& Cycles, Mem& memory) {
+		Byte Data = memory[ProgramCounter];
+		ProgramCounter++;
+		Cycles--;
+		return Data;
+	}
+
+	//opcaodes
+	static constexpr Byte
+		INS_LDA_IM = 0xA9;
+
+	void Execute(u32 Cycles, Mem& memory) {
+		while (Cycles > 0) {
+			Byte Instruction = FetchByte(Cycles, memory);
+			switch (Instruction) {
+				case INS_LDA_IM: {
+					Byte value =
+						FetchByte(Cycles, memory);
+					A = value;
+					Z = (A == 0);
+					N = (A & 0b10000000) > 0;
+				}break;
+				default: {
+					printf("Instruction not handled %d", Instruction);
+				}break;
+			}
+		}
+	}
+};
+
+int main(){
+	Mem mem;
+	CPU cpu;
+	cpu.Reset( mem );
+	//inline a little program
+	mem[0xFFFC] = CPU::INS_LDA_IM;
+	mem[0xFFFD] = 0x42;
+	//end - ionline a little program
+	cpu.Execute(2, mem);
+	return 0;
+}
